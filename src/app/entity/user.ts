77c89f78b2b3ddcacb06ebff291f1de1667f4ce4
Olivier Barnerias <olivier.barnerias@hotmail.fr>
2019-07-17 13:52:03 +0200

    update entity

---
+++ src/app/entity/user.ts
@@ -0,0 +1,13 @@
+export class user{
+  id: number;
+  login: string;
+  password: string;
+  email: string;
+
+
+  constructor(login: string, password: string, email: string) {
+    this.login = login;
+    this.password = password;
+    this.email = email;
+  }
+}
