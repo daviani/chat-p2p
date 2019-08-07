a0b5c53a0a87642f10e0dc0246b3389d452fcde1
antresh <antresh@yandex.com>
2019-08-07 14:55:08 +0200

    Intergation with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { ChatService } from './services/chat.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   NbButtonModule,
   NbCardModule,
@@ -23,6 +25,7 @@ import { RegisterComponent } from './register/register.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ViewMessageComponent } from './view-message/view-message.component';
 import { InputBarComponent } from './input-bar/input-bar.component';
+
 
 @NgModule({
   declarations: [
@@ -49,8 +52,10 @@ import { InputBarComponent } from './input-bar/input-bar.component';
     NbCardModule,
     NbSearchModule,
     NbButtonModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
